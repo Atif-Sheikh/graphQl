57a1c1e5b0134148a85338ba195e0fce92deee1b
Mohammad Mohsin <mmohsinkhalid@outlook.com>
2018-12-10 14:38:15 +0500

    GraphQl server implemented

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const app = express();
+
+
+mongoose.connection.once('open',() => {
+    console.log('connected to database')
+})
+app.use(cors())
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+const PORT = 4000;
+app.listen(PORT,() => {
+    console.log(`now listining port ${PORT}`)
+})
